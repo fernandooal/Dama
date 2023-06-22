0550fef0bdc99dfd2a906ee1444634feb8dc06c7
Fernando <120701366+fernandooal@users.noreply.github.com>
2023-06-22 19:52:08 -0300

    Update Peca.java

--- Peca.java
+++ Peca.java
@@ -2,7 +2,10 @@ package br.com.unicuritiba.dama.model;
 
 import java.util.LinkedList;
 
+//conceito de classe abstrata, o que impossibilita de criar instâncias dessa classe, mas libera para fazer isso a partir das classes filhas,
+//o que permite utilizar o metodo de mexer dama da classe filha no tabuleiro.
 public abstract class Peca {
+	// atributos necessarios
 	protected int xPeca;
 	protected int yPeca;
 	protected int x;
@@ -12,6 +15,7 @@ public abstract class Peca {
 	protected boolean dama;
 	private boolean comeuPeca;
 
+	// construtor
 	public Peca(int xPeca, int yPeca, boolean vermelho, LinkedList<Peca> pecaLista, boolean dama, boolean comeuPeca) {
 		this.vermelho = vermelho;
 		this.xPeca = xPeca;
@@ -25,6 +29,7 @@ public abstract class Peca {
 		pecaLista.add(this);
 	}
 
+	// getters and setters
 	public boolean isComeuPeca() {
 		return comeuPeca;
 	}
@@ -89,12 +94,18 @@ public abstract class Peca {
 		this.pecaLista = pecaLista;
 	}
 
+	// método para movimentar a peça
 	public final void mexerPeca(int xPeca, int yPeca) {
-		// Verifica se o movimento é diagonal (uma casa na vertical e uma casa na
-		// horizontal)
+		// verificando a cor pois, diferente da dama, a peça normal não pode voltar,
+		// então o y para as pretas e para as vermelhas se difere entre positivo e
+		// negativo
 		if (isVermelho() == true) {
+			// verificando se a peça se movimentou horizontalmente para qualquer lado e
+			// verticalmente para baixo (para cima a peça vermelha não pode)
 			if (Math.abs(this.xPeca - xPeca) == 1 && (this.yPeca - yPeca) == -1) {
 				boolean casaVazia = true;
+				// passando pela lista inteira de peças para verificar se não existe nenhuma
+				// outra ocupando a posição
 				for (Peca p : pecaLista) {
 					if (p.getxPeca() == xPeca && p.getyPeca() == yPeca) {
 						casaVazia = false;
@@ -102,24 +113,32 @@ public abstract class Peca {
 					}
 				}
 
+				// se a casa estiver vazia, a peça conseguirá se movimentar
 				if (casaVazia) {
 					this.xPeca = xPeca;
 					this.yPeca = yPeca;
 					x = xPeca * 64;
 					y = yPeca * 64;
 				} else {
+					// caso contrário, resetará a posição
 					resetarPosicao();
 				}
+				// verificando a mesma coisa do if passado, mas andando 2 casas no x e no y para
+				// comer uma peça
 			} else if (Math.abs(this.xPeca - xPeca) == 2 && Math.abs(this.yPeca - yPeca) == 2) {
 				boolean casaVazia = true;
-				int pecaComidaX = (this.xPeca + xPeca) / 2; // Coordenada X da peça a ser comida
-				int pecaComidaY = (this.yPeca + yPeca) / 2; // Coordenada Y da peça a ser comida
+				// coordenada x e y da peça a ser comida
+				int pecaComidaX = (this.xPeca + xPeca) / 2;
+				int pecaComidaY = (this.yPeca + yPeca) / 2;
 
+				// passando pela lista verificando se tem alguma peça nas coordenadas que não
+				// seja vermelha
 				for (Peca p : pecaLista) {
 					if (p.getxPeca() == pecaComidaX && p.getyPeca() == pecaComidaY) {
-						if(p.isVermelho() == false) {
-							p.removerPeca(); // Remove a peça que será comida
-							p.setComeuPeca(true);;
+						if (p.isVermelho() == false) {
+							p.removerPeca();
+							p.setComeuPeca(true);
+							;
 						} else {
 							casaVazia = false;
 						}
@@ -127,18 +146,23 @@ public abstract class Peca {
 					}
 				}
 
+				// se a casa estiver vazia, a peça se movimentará
 				if (casaVazia) {
 					this.xPeca = xPeca;
 					this.yPeca = yPeca;
 					x = xPeca * 64;
 					y = yPeca * 64;
 				} else {
+					// caso contrário, reseta a posição
 					resetarPosicao();
 				}
 			} else {
+				// se a distância movimentada for incompatível com as regras do jogo, resetará a
+				// posição
 				resetarPosicao();
 			}
 		} else {
+			// mesmo processo para a cor preta
 			if (Math.abs(this.xPeca - xPeca) == 1 && (this.yPeca - yPeca) == 1) {
 				boolean casaVazia = true;
 				for (Peca p : pecaLista) {
@@ -163,7 +187,7 @@ public abstract class Peca {
 
 				for (Peca p : pecaLista) {
 					if (p.getxPeca() == pecaComidaX && p.getyPeca() == pecaComidaY) {
-						if(p.isVermelho()) {
+						if (p.isVermelho()) {
 							p.removerPeca(); // Remove a peça que será comida
 							p.setComeuPeca(true);
 						} else {
@@ -187,13 +211,16 @@ public abstract class Peca {
 		}
 	}
 
+	// criando um metodo abstrato para poder utilizar da classe filha
 	public abstract void mexerDama(int xPeca, int yPeca);
 
+	// método de resetar a posição caso o movimento seja inválido
 	public void resetarPosicao() {
 		this.x = this.xPeca * 64;
 		this.y = this.yPeca * 64;
 	}
 
+	// método de remover a peça da lista e consequentemente do jogo
 	public void removerPeca() {
 		pecaLista.remove(this);
 	}
