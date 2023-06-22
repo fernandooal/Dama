324d62f4f7aaf2e0afb15242f244243ee3834978
Fernando <120701366+fernandooal@users.noreply.github.com>
2023-06-22 19:53:00 -0300

    Update Jogo.java

--- Jogo.java
+++ Jogo.java
@@ -7,13 +7,15 @@ import br.com.unicuritiba.dama.model.*;
 public class Jogo {
 
 	public static void main(String[] args) {
-		
+
+		// criando instâncias de Jogador e definindo seus nomes
 		Jogador jogador1 = new Jogador();
 		Jogador jogador2 = new Jogador();
-		
-		jogador1.setNome(JOptionPane.showInputDialog("Digite o nome do jogador 1 (peças vermelhas):" ));
-		jogador2.setNome(JOptionPane.showInputDialog("Digite o nome do jogador 2 (peças pretas):" ));
-		
+
+		jogador1.setNome(JOptionPane.showInputDialog("Digite o nome do jogador 1 (peças vermelhas):"));
+		jogador2.setNome(JOptionPane.showInputDialog("Digite o nome do jogador 2 (peças pretas):"));
+
+		// iniciando o jogo pelo método do Tabuleiro
 		Tabuleiro.comecarJogo(jogador1, jogador2);
 	}
 }
